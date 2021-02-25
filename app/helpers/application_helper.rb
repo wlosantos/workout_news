dccd2fa35a4a7714aca253a9e800d80038339b9c
Wendel Lopes <wendelopes@msn.com>
2021-02-25 11:10:10 -0300

    Styled post show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
   def get_image(post)
-
     if post.image.attached?
       image = post.image
     else
       image = "base_education.jpg"
     end
-    image_tag image
+    image_tag image, class: 'adjust_image'
   end
+
 end
